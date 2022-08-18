cd340645ebb18bace1a329ff877633a1df3614b8
Rajesh-Royal <rajeshroyal896@gmail.com>
2022-08-18 19:33:07 +0530

    feat: catalog detail page component created

--- src/pages/CatalogDetailPage.tsx
+++ src/pages/CatalogDetailPage.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getCatalogDetail } from "../api/services/GetCatalogDetail";
+import { ICatalog } from "../api/services/GetCatalogLists";
+import CatalogDetail from "../components/catalog-detail/CatalogDetail";
 
 const CatalogDetailPage = () => {
     const urlParams = useParams();
-    console.log('urlParams', urlParams)
+    const [catalogData, setCatalogData] = useState<ICatalog>()
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        if (urlParams.productId) {
+            getCatalogDetail(urlParams.productId).then((response) => {
+                setLoading(false);
+                setCatalogData(response.data);
+            }).catch((error) => {
+                setLoading(false);
+            })
+        }
+    }, [urlParams.productId])
     return (
-        <div>Product details</div>
+        loading ?
+            <p>Loading............</p> :
+            <CatalogDetail catalogData={catalogData} />
     )
 }
 
