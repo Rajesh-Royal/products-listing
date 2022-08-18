a11d8903310576dde112ce0a0668699038ce7b32
Rajesh-Royal <rajeshroyal896@gmail.com>
2022-08-18 19:42:31 +0530

    style: catalog details css added

--- src/components/catalog-detail/CatalogDetail.tsx
+++ src/components/catalog-detail/CatalogDetail.tsx
@@ -1,11 +1,12 @@
-import { ICatalog } from '../../api/services/GetCatalogLists'
+import { ICatalog } from '../../api/services/GetCatalogLists';
+import "./CatalogDetail.scss";
 
 const CatalogDetail = ({ catalogData }: { catalogData: ICatalog | undefined }) => {
     return (
         <div className="catalog-details">
             {catalogData ?
                 <>
-                    <p className="title">{catalogData?.Title}</p>
+                    <h2 className="title">{catalogData?.Title}</h2>
                     <p className="maker">{catalogData?.Maker}</p>
                     <p className="description">{catalogData?.Description}</p>
                     {catalogData?.img ? <img src={catalogData.img} alt="catalog preview" className="preview-img" /> : null}
