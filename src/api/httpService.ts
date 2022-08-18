a230a56a36071397adccb3cac78931f12a875357
Rajesh-Royal <rajeshroyal896@gmail.com>
2022-08-18 19:03:23 +0530

    feat: axios installed and httpservice created

--- src/api/httpService.ts
+++ src/api/httpService.ts
@@ -1,1 +1,17 @@
-export const empty = { }
+import axios, { AxiosResponse } from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_API_URL
+});
+
+
+export default class HTTPService {
+    static get<T = never, R = AxiosResponse<T>>(url: string, params?: any): Promise<R> {
+        return new Promise((resolve, reject) => {
+            axiosInstance
+                .get(url, params)
+                .then((response) => resolve(response.data))
+                .catch((error) => reject(error.response || error));
+        });
+    }
+}
